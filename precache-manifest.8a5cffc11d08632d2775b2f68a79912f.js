0e07d420c571e208e09558dabdc206919b47e09d
vantanhung2000 <vantanhung2000@gmail.com>
2019-08-02 16:38:07 +0700

    Updates

---
+++ precache-manifest.8a5cffc11d08632d2775b2f68a79912f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fb769330301352620dc9732c4b1577",
+    "url": "/demo_deploy/index.html"
+  },
+  {
+    "revision": "5cf15085d2babedaf125",
+    "url": "/demo_deploy/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/demo_deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5cf15085d2babedaf125",
+    "url": "/demo_deploy/static/js/main.97c0d8c9.chunk.js"
+  },
+  {
+    "revision": "cd2f6f41065bb887093a",
+    "url": "/demo_deploy/static/js/runtime~main.427d9973.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);

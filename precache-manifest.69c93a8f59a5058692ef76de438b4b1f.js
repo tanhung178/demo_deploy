ed4ca249e8ec0dbf3737d72d718f06730d581259
vantanhung2000 <vantanhung2000@gmail.com>
2019-08-02 15:31:12 +0700

    build

---
+++ precache-manifest.69c93a8f59a5058692ef76de438b4b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0432d51ddf2c1de00262aad8e65cab",
+    "url": "/demo_deploy/index.html"
+  },
+  {
+    "revision": "38414b56280522e1d89f",
+    "url": "/demo_deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/demo_deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "38414b56280522e1d89f",
+    "url": "/demo_deploy/static/js/main.97c0d8c9.chunk.js"
+  },
+  {
+    "revision": "cd2f6f41065bb887093a",
+    "url": "/demo_deploy/static/js/runtime~main.427d9973.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
